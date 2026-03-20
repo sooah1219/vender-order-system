77c728b5a7adeebaa89254aa5ca22b881fd125b7
Sooah <gcho14@my.bcit.ca>
2026-03-20 13:16:43 -0700

    postgreSQL

--- app/database.py
+++ app/database.py
@@ -1,8 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./app.db"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {}
+)
+
 SessionLocal = sessionmaker(bind=engine)
+
+
 class Base(DeclarativeBase):
-  pass
+    pass
